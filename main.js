e634b850285ec0251f5641a3df04df5e24fe4f9f
¨yunusemre¨ <ttnkntnsmr69@gmail.com>
2022-12-21 21:58:48 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// ısım soysıım bırlestorme
+
+var Name = 'Yunus Emre';
+var surName = 'Tutuneken';
+
+//console.log(Name + ' ' + surName);
+
+document.write(Name +' '+ surName + '</br>');
+
+//1 den 100 arasında cıft sayıları yazdırma
+
+for(let i = 1; i < 100; i++){
+
+    if(i % 2 == 0){
+        document.writeln(i + '</br>');
+    }
+    
+}
